c722d508d22413ef4eb8e39205044cefb00a430d
Nova1545 <aidencw01@gmail.com>
2021-12-08 13:20:08 -0500

    Fixed debug dump file path

--- NovaAPI/Controllers/MediaController.cs
+++ NovaAPI/Controllers/MediaController.cs
@@ -289,7 +289,7 @@ namespace NovaAPI.Controllers
             {
                 MemoryStream ms = new();
                 file.OpenReadStream().CopyTo(ms);
-                FileStream debugDumb = System.IO.File.OpenWrite(GlobalUtils.RootDebug + DateTime.Now + ".log");
+                FileStream debugDumb = System.IO.File.OpenWrite($"{GlobalUtils.RootDebug}/{DateTime.Now}.log");
                 ms.CopyTo(debugDumb);
                 debugDumb.Close();
                 Image m = Image.FromStream(ms);
