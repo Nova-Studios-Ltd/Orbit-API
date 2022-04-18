fd8fa556abc49d8f992a7b574c02ea603a5f4078
Aiden Wilkins <aidencw01@gmail.com>
2022-04-17 21:28:27 -0400

    Removing old endpoints

--- NovaAPI/Controllers/UserController.cs
+++ NovaAPI/Controllers/UserController.cs
@@ -89,23 +89,6 @@ namespace NovaAPI.Controllers
             return StatusCode(404, "Unable to find yourself");
         }
         
-        [HttpPatch("{user_uuid}/Username")]
-        [Obsolete("Please use User/@me/Username")]
-        [TokenAuthorization]
-        public ActionResult ChangeUsername(string user_uuid, [FromBody] string username)
-        {
-            if (string.IsNullOrEmpty(username)) return StatusCode(400);
-            if (!Context.UserExsists(user_uuid)) return StatusCode(404);
-            using MySqlConnection conn = Context.GetUsers();
-            conn.Open();
-            using MySqlCommand cmd = new($"UPDATE Users SET Username=@user WHERE (UUID=@uuid) AND (Token=@token)", conn);
-            cmd.Parameters.AddWithValue("@uuid", user_uuid);
-            cmd.Parameters.AddWithValue("@user", username);
-            cmd.Parameters.AddWithValue("@token", this.GetToken());
-            cmd.ExecuteNonQuery();
-            return StatusCode(200);
-        }
-        
         [HttpPatch("@me/Username")]
         [TokenAuthorization]
         public ActionResult ChangeUsername([FromBody] string username)
@@ -119,29 +102,6 @@ namespace NovaAPI.Controllers
             cmd.Parameters.AddWithValue("@uuid", user_uuid);
             cmd.Parameters.AddWithValue("@user", username);
             cmd.Parameters.AddWithValue("@token", this.GetToken());
-            cmd.ExecuteNonQuery();
-            return StatusCode(200);
-        }
-
-        
-        [HttpPatch("{user_uuid}/Password")]
-        [Obsolete("Please use User/@me/Password")]
-        [TokenAuthorization]
-        public ActionResult ChangePassword(string user_uuid, PasswordUpdate update)
-        {
-            if (string.IsNullOrEmpty(update.Password)) return StatusCode(400);
-            if (!Context.UserExsists(user_uuid)) return StatusCode(404);
-            dynamic u = RetUser(user_uuid).Value;
-            byte[] salt = EncryptionUtils.GetSalt(64);
-            using MySqlConnection conn = Context.GetUsers();
-            using MySqlCommand cmd = new($"UPDATE Users SET Password=@pass,Salt=@salt,Token=@newToken,PrivKey=@key,IV=@iv WHERE (UUID=@uuid) AND (Token=@token)", conn);
-            cmd.Parameters.AddWithValue("@uuid", user_uuid);
-            cmd.Parameters.AddWithValue("@pass", EncryptionUtils.GetSaltedHashString(update.Password, salt));
-            cmd.Parameters.AddWithValue("@salt", salt);
-            cmd.Parameters.AddWithValue("@token", this.GetToken());
-            cmd.Parameters.AddWithValue("@newToken", EncryptionUtils.GetSaltedHashString(user_uuid + u.Email + update.Password + u.Username + DateTime.Now.ToString(), EncryptionUtils.GetSalt(8)));
-            cmd.Parameters.AddWithValue("@privKey", update.Key.Content);
-            cmd.Parameters.AddWithValue("@iv", update.Key.IV);
             cmd.ExecuteNonQuery();
             return StatusCode(200);
         }
@@ -167,25 +127,7 @@ namespace NovaAPI.Controllers
             cmd.ExecuteNonQuery();
             return StatusCode(200);
         }
-
-
-        [HttpPatch("{user_uuid}/Email")]
-        [Obsolete("Please use User/@me/Email")]
-        [TokenAuthorization]
-        public ActionResult ChangeEmail(string user_uuid, [FromBody] string email)
-        {
-            if (string.IsNullOrEmpty(email)) return StatusCode(400);
-            if (!Context.UserExsists(user_uuid)) return StatusCode(404);
-            using MySqlConnection conn = Context.GetUsers();
-            conn.Open();
-            using MySqlCommand cmd = new($"UPDATE Users SET Email=@email WHERE (UUID=@uuid) AND (Token=@token)", conn);
-            cmd.Parameters.AddWithValue("@uuid", user_uuid);
-            cmd.Parameters.AddWithValue("@email", email);
-            cmd.Parameters.AddWithValue("@token", this.GetToken());
-            cmd.ExecuteNonQuery();
-            return StatusCode(200);
-        }
-
+        
         [HttpPatch("@me/Email")]
         [TokenAuthorization]
         public ActionResult ChangeEmail([FromBody] string email)
@@ -202,23 +144,7 @@ namespace NovaAPI.Controllers
             cmd.ExecuteNonQuery();
             return StatusCode(200);
         }
-            
 
-        [HttpPost("/Login")]
-        [Obsolete("Please use /Auth/Login instead")]
-        public ActionResult<ReturnLoginUserInfo> LoginUser(LoginUserInfo info)
-        {
-            return StatusCode(410, "Please use /Auth/Login");
-        }
-
-        [HttpPost("/Register")]
-        [Obsolete("Please use /Auth/Register instead")]
-        public ActionResult<User> RegisterUser(CreateUserInfo info)
-        {
-            return StatusCode(410, "Please use /Auth/Register");
-        }
-        
-        
         [HttpGet("{username}/{discriminator}/UUID")]
         public ActionResult<string> GetUserUUIDFromUsername(string username, string discriminator)
         {
@@ -239,42 +165,7 @@ namespace NovaAPI.Controllers
             }
             return StatusCode(404, $"Unable to find user {username}#{discriminator}");
         }
-
         
-        [HttpDelete("{user_uuid}")]
-        [Obsolete("Please use User/@me")]
-        [TokenAuthorization]
-        public ActionResult DeleteUser(string user_uuid)
-        {
-            using (MySqlConnection conn = Context.GetUsers())
-            {
-                conn.Open();
-                using MySqlCommand removeUser = new($"DELETE FROM Users WHERE (UUID=@uuid) AND (Token=@token)", conn);
-                removeUser.Parameters.AddWithValue("@uuid", user_uuid);
-                removeUser.Parameters.AddWithValue("@token", this.GetToken());
-                if (removeUser.ExecuteNonQuery() == 0) return StatusCode(404);
-
-                using MySqlConnection keystoreUser = Context.GetUsers();
-                keystoreUser.Open();
-                using MySqlCommand keystore = new($"SELECT * FROM `{user_uuid}_keystore`", keystoreUser);
-                MySqlDataReader reader = keystore.ExecuteReader();
-                while (reader.Read())
-                {
-                    using MySqlCommand cmd = new($"DELETE FROM `{reader["UUID"].ToString()}_keystore` WHERE (UUID=@uuid)", conn);
-                    cmd.Parameters.AddWithValue("@uuid", user_uuid);
-                    Event.KeyAddedToKeystore(reader["UUID"].ToString(), user_uuid);
-                    cmd.ExecuteNonQuery();
-                }
-                keystoreUser.Close();
-                
-                StorageUtil.DeleteFile(StorageUtil.MediaType.Avatar, user_uuid);
-                
-                using MySqlCommand removeUserAccess = new($"DROP TABLE `{user_uuid}`, `{user_uuid}_keystore`", conn);
-                removeUserAccess.ExecuteNonQuery();
-            }
-            return StatusCode(200);
-        }
-
         [HttpDelete("@me")]
         [TokenAuthorization]
         public ActionResult DeleteUser()
@@ -308,39 +199,6 @@ namespace NovaAPI.Controllers
             return StatusCode(200);
         }
         
-        
-        [HttpGet("{user_uuid}/Keystore")]
-        [Obsolete("Please use User/@me/Keystore")]
-        [TokenAuthorization]
-        public ActionResult<Dictionary<string, string>> GetKeystore(string user_uuid)
-        {
-            Response.Redirect($"/User/@me/Keystore");
-            return StatusCode(302);
-        }
-
-        [HttpGet("{user_uuid}/Keystore/{key_user_uuid}")]
-        [Obsolete("Please use User/@me/Keystore/{key_user_uuid}")]
-        [TokenAuthorization]
-        public ActionResult<string> GetKey(string user_uuid, string key_user_uuid)
-        {
-            Response.Redirect($"/User/@me/{key_user_uuid}");
-            return StatusCode(302);
-        }
-
-
-        [HttpPost("{user_uuid}/Keystore/{key_user_uuid}")]
-        [Obsolete("Please user User/@me/Keystore/{key_user_uuid}")]
-        [TokenAuthorization]
-        public ActionResult SetKey(string user_uuid, string key_user_uuid, [FromBody] string key)
-        {
-            Response.Redirect($"/User/@me/{key_user_uuid}");
-            Response.Body.Write(Encoding.UTF8.GetBytes(key));
-            return StatusCode(302);
-        }
-
-
-        // TODO Remove Channels
-        [HttpGet("Channels")]
         [HttpGet("@me/Channels")]
         [TokenAuthorization]
         public ActionResult<List<string>> GetUserChannels()
