859a6f6cd57d2654e861f5b69261e471b9db7d6b
Nova1545 <aidencw01@gmail.com>
2022-07-18 13:07:33 -0400

    Removed arg

--- NovaAPI/Controllers/MediaController.cs
+++ NovaAPI/Controllers/MediaController.cs
@@ -190,7 +190,6 @@ namespace NovaAPI.Controllers
         [HttpPost("/proxy")]
         public async Task<ActionResult> PostProxyURL(string url)
         {
-            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;  
             HttpClient client = new HttpClient();
             foreach (string key in Request.Headers.Keys)
             {
